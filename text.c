84c6cebc6345a0636f71e7e134ca60cfafbd69c4
呂鴻祥 <lvhongxiang@pc134220.ocu.edu.tw>
2023-12-11 09:24:52 +0800

    V1211-01

---
+++ text.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(int argc,const char*argv[]){
+    char a ='1',b ='4';
+    printf("%c",b);
+    printf("%d\n",b-a);
+    return 0;
+}
+
+
+windows 32位元 C++
+void Func (char str[100]){
+    sizeof(str) = ?
+}
+
+void*p =malloc(100)
+sizeof (p)= ?
+
+sizeof(str)與sizeof(p)的值
+
+int main(int argc,const char*argv[]){
+    int x;
+    int y;
+    x =4;
+    y = x+1/2;
+}
